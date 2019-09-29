efe9d6e74cbc38655294b3d2ada1b0f253b56dd1
Yash J Shetty <yash17shetty@gmail.com>
2019-09-28 23:53:29 -0400

    ys - able to parse most or reqs

--- catalog_scraper.js
+++ catalog_scraper.js
@@ -1,6 +1,8 @@
 var rp = require('request-promise');
 var cheerio = require('cheerio');
 
+//BUGS:
+// - ENGW 3302 or ENGW3315 show up as separate orRequirements.
 var options = {
     uri: 'http://catalog.northeastern.edu/archive/2018-2019/undergraduate/computer-information-science/computer-science/bscs/#programrequirementstext',
     transform: function (body) {
@@ -11,23 +13,53 @@ var options = {
 rp(options)
 	.then(function ($) {
         var courseData = []
+        var sectionIsOrReq = false
+        var orRequirement = "";
         $('#programrequirementstextcontainer table.sc_courselist tr').each(function() {
         	var currentRow = $(this);
-            
+            if (currentRow.find("span.areaheader").length !== 0) {
+                //add the previous orRequirement to the course data
+                if (orRequirement !== "") {
+                    courseData.push(orRequirement);
+                    orRequirement = "";
+                    sectionIsOrReq = false;
+                }   
+                let commentSpan = $(this);
+                courseData.push(commentSpan.text());
+            }
+            if (currentRow.find("span.courselistcomment").length !== 0) {
+                currentRow.find("span.courselistcomment").each(function() {
+                    let commentSpan = $(this);
+                    if (commentSpan.text().includes("Complete")) {
+                        sectionIsOrReq = true;
+                    }
+                });
+            }
             if (currentRow.find("td.codecol a").length !== 0) {
-                var requirement = "";
+                var andRequirement = "";
                 currentRow.find("td.codecol a").each(function() {
                     let currentAnchor = $(this)
                     let courseNum = currentAnchor.text()
-                    if (requirement !== "") {
+                    if (andRequirement !== "") {
                         courseNum = " and " + courseNum
                     }
-                    requirement += courseNum
+                    andRequirement += courseNum
                 });
-                courseData.push(requirement);
+                if (sectionIsOrReq) {
+                        if (orRequirement !== "") {
+                            orRequirement += " or " + andRequirement;
+                        }
+                        else {
+                            orRequirement = andRequirement;
+                        }
+                } else {
+                        courseData.push(andRequirement);
+                }
             }
         });
-        
+        if (orRequirement !== "") {
+            courseData.push(orRequirement);
+        }
         console.log(courseData);
     })
     .catch(function (err) {
