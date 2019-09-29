4cc4436d2eb5761467b13881f26f2f2889612fdd
Yash J Shetty <yash17shetty@gmail.com>
2019-09-29 00:45:05 -0400

    ys - get course ranges

--- catalog_scraper.js
+++ catalog_scraper.js
@@ -15,10 +15,12 @@ rp(options)
         var major = []
         var sectionReq = []
         var sectionIsOrReq = false
+        var sectionIsRange = false
         var orRequirement = "";
         $('#programrequirementstextcontainer table.sc_courselist tr').each(function() {
         	var currentRow = $(this);
             if (currentRow.find("span.areaheader").length !== 0) {
+                sectionIsRange = false
                 //add the previous orRequirement to the course data
                 if (orRequirement !== "") {
                     sectionReq.push(orRequirement);
@@ -35,30 +37,48 @@ rp(options)
             if (currentRow.find("span.courselistcomment").length !== 0) {
                 currentRow.find("span.courselistcomment").each(function() {
                     let commentSpan = $(this);
+                    //should use regExp matching here
                     if (commentSpan.text().includes("Complete")) {
                         sectionIsOrReq = true;
                     }
+                    if (commentSpan.text().includes("Complete 8 credits of CS, IS or DS classes that are not already required. Choose courses within the following ranges")) {
+                        sectionIsRange = true;
+                    }
                 });
             }
-            if (currentRow.find("td.codecol a").length !== 0) {
-                var andRequirement = "";
-                currentRow.find("td.codecol a").each(function() {
-                    let currentAnchor = $(this)
-                    let courseNum = currentAnchor.text()
-                    if (andRequirement !== "") {
-                        courseNum = " and " + courseNum
+            if (sectionIsRange) {
+                if (currentRow.find("span.courselistcomment").length !== 0) {
+                    let commentSpan = $(this);
+                    let range = [];
+                    commentSpan.find("a").each(function() {
+                        let currentAnchor = $(this)
+                        let courseNum = currentAnchor.text()
+                        range.push(courseNum);
+                    });
+                    rangeString = range.join(" - ");
+                    sectionReq.push(rangeString);
+                }
+            } else {
+                if (currentRow.find("td.codecol a").length !== 0) {
+                    var andRequirement = "";
+                    currentRow.find("td.codecol a").each(function() {
+                        let currentAnchor = $(this)
+                        let courseNum = currentAnchor.text()
+                        if (andRequirement !== "") {
+                            courseNum = " and " + courseNum
+                        }
+                        andRequirement += courseNum
+                    });
+                    if (sectionIsOrReq) {
+                        if (orRequirement !== "") {
+                            orRequirement += " or " + andRequirement;
+                        }
+                        else {
+                            orRequirement = andRequirement;
+                        }
+                    } else {
+                        sectionReq.push(andRequirement);
                     }
-                    andRequirement += courseNum
-                });
-                if (sectionIsOrReq) {
-                    if (orRequirement !== "") {
-                        orRequirement += " or " + andRequirement;
-                    }
-                    else {
-                        orRequirement = andRequirement;
-                    }
-                } else {
-                    sectionReq.push(andRequirement);
                 }
             }
         });
