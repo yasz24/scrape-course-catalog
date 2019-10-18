4a7f26d5f9742d809ba4d19ed1e1959dcb4217fb
Yash J Shetty <yash17shetty@gmail.com>
2019-10-18 15:56:18 -0400

    ys - organized functions for code review

--- catalog_scraper.js
+++ catalog_scraper.js
@@ -6,116 +6,125 @@ var cheerio = require('cheerio');
 
 var parseMajorData = require('./parseMajor');
 
-
-function scrapeMajorDataFromCatalog(link) {
+function catalogToMajor(link) {
     var options = {
         uri: link,
         transform: function (body) {
             return cheerio.load(body);
         }
     };
-
     rp(options)
-        .then(function ($) {
-            var major = []
-            var sectionReq = []
-            var sectionIsOrReq = false
-            var sectionIsRange = false
-            var orRequirement = "";
-            var hoursReq = "";
-            $('#programrequirementstextcontainer table.sc_courselist tr').each(function() {
-                var currentRow = $(this);
-                if (currentRow.find("span.areaheader").length !== 0) {
-                    sectionIsRange = false
-                    hoursReq = "";
-                    //add the previous orRequirement to the course data
-                    if (orRequirement !== "") {
-                        sectionReq.push(orRequirement);
-                        orRequirement = "";
-                        sectionIsOrReq = false;
-                    }
-                    if (sectionReq.length !== 0) {
-                        major.push(sectionReq)
-                        sectionReq = []
-                    }   
-                    let commentSpan = $(this);
-                    sectionReq.push(commentSpan.text());
-                }
-                if (currentRow.find("span.courselistcomment").length !== 0) {
-                    currentRow.find("span.courselistcomment").each(function() {
-                        let commentSpan = $(this);
-                        //should use regExp matching here
-                        if (commentSpan.text().includes("Complete")) {
-                            sectionIsOrReq = true;
-                            hoursReq = currentRow.find("td.hourscol").text();
-                        }
-                        if (commentSpan.text().includes("Complete 8 credits of CS, IS or DS classes that are not already required. Choose courses within the following ranges")) {
-                            sectionIsRange = true;
-                            hoursReq = currentRow.find("td.hourscol").text();
-                        }
-                    });
-                }
-                if (sectionIsRange) {
-                    if (sectionReq.length === 1) {
-                        sectionReq.push("RANGE");
-                        sectionReq.push(hoursReq);
-                    }
-                    if (currentRow.find("span.courselistcomment").length !== 0) {
-                        let commentSpan = $(this);
-                        let range = [];
-                        commentSpan.find("a").each(function() {
-                            let currentAnchor = $(this)
-                            let courseNum = currentAnchor.text()
-                            range.push(courseNum);
-                        });
-                        if (range.length !== 0) {
-                            rangeString = range.join("-");
-                            sectionReq.push(rangeString);
-                        }
-                    }
-                } else {
-                    if (currentRow.find("td.codecol a").length !== 0) {
-                        var andRequirement = "";
-                        currentRow.find("td.codecol a").each(function() {
-                            let currentAnchor = $(this)
-                            let courseNum = currentAnchor.text()
-                            if (andRequirement !== "") {
-                                courseNum = "%and%" + courseNum
-                            }
-                            andRequirement += courseNum
-                        });
-                        if (sectionIsOrReq) {
-                            if (sectionReq.length === 1) {
-                                sectionReq.push("OR");
-                                sectionReq.push(hoursReq);
-                            }
-                            if (orRequirement !== "") {
-                                orRequirement += "%or%" + andRequirement;
-                            }
-                            else {
-                                orRequirement = andRequirement;
-                            }
-                        } else {
-                            if (sectionReq.length === 1) {
-                                sectionReq.push("AND");
-                            }
-                            sectionReq.push(andRequirement);
-                        }
-                    }
-                }
-            });
-            if (orRequirement !== "") {
-                sectionReq.push(orRequirement);
-            }
-            if (sectionReq.length !== 0) {
-                major.push(sectionReq);
-                sectionReq = []
-            }  
-            console.log(parseMajorData(major));
+        .then(($) => scrapeMajorDataFromCatalog($))
+        .then((scrapedMajor) => {
+            //write the parse major data to database?
+            let parsedMajorObject = parseMajorData(scrapedMajor);
+            console.log("--------------------Parsed major object--------------------");
+            console.log(parsedMajorObject);
         })
         .catch(function (err) {
             console.log(err)
         });
 }
 
-scrapeMajorDataFromCatalog('http://catalog.northeastern.edu/archive/2018-2019/undergraduate/computer-information-science/computer-science/bscs/#programrequirementstext')
+function scrapeMajorDataFromCatalog($) {
+    return new Promise((resolve, reject) => {
+        var major = []
+        var sectionReq = []
+        var sectionIsOrReq = false
+        var sectionIsRange = false
+        var orRequirement = "";
+        var hoursReq = "";
+        $('#programrequirementstextcontainer table.sc_courselist tr').each(function() {
+            var currentRow = $(this);
+            if (currentRow.find("span.areaheader").length !== 0) {
+                sectionIsRange = false
+                hoursReq = "";
+                //add the previous orRequirement to the course data
+                if (orRequirement !== "") {
+                    sectionReq.push(orRequirement);
+                    orRequirement = "";
+                    sectionIsOrReq = false;
+                }
+                if (sectionReq.length !== 0) {
+                    major.push(sectionReq)
+                    sectionReq = []
+                }   
+                let commentSpan = $(this);
+                sectionReq.push(commentSpan.text());
+            }
+            if (currentRow.find("span.courselistcomment").length !== 0) {
+                currentRow.find("span.courselistcomment").each(function() {
+                    let commentSpan = $(this);
+                    //should use regExp matching here
+                    if (commentSpan.text().includes("Complete")) {
+                        sectionIsOrReq = true;
+                        hoursReq = currentRow.find("td.hourscol").text();
+                    }
+                    if (commentSpan.text().includes("Complete 8 credits of CS, IS or DS classes that are not already required. Choose courses within the following ranges")) {
+                        sectionIsRange = true;
+                        hoursReq = currentRow.find("td.hourscol").text();
+                    }
+                });
+            }
+            if (sectionIsRange) {
+                if (sectionReq.length === 1) {
+                    sectionReq.push("RANGE");
+                    sectionReq.push(hoursReq);
+                }
+                if (currentRow.find("span.courselistcomment").length !== 0) {
+                    let commentSpan = $(this);
+                    let range = [];
+                    commentSpan.find("a").each(function() {
+                        let currentAnchor = $(this)
+                        let courseNum = currentAnchor.text()
+                        range.push(courseNum);
+                    });
+                    if (range.length !== 0) {
+                        rangeString = range.join("-");
+                        sectionReq.push(rangeString);
+                    }
+                }
+            } else {
+                if (currentRow.find("td.codecol a").length !== 0) {
+                    var andRequirement = "";
+                    currentRow.find("td.codecol a").each(function() {
+                        let currentAnchor = $(this)
+                        let courseNum = currentAnchor.text()
+                        if (andRequirement !== "") {
+                            courseNum = "%and%" + courseNum
+                        }
+                        andRequirement += courseNum
+                    });
+                    if (sectionIsOrReq) {
+                        if (sectionReq.length === 1) {
+                            sectionReq.push("OR");
+                            sectionReq.push(hoursReq);
+                        }
+                        if (orRequirement !== "") {
+                            orRequirement += "%or%" + andRequirement;
+                        }
+                        else {
+                            orRequirement = andRequirement;
+                        }
+                    } else {
+                        if (sectionReq.length === 1) {
+                            sectionReq.push("AND");
+                        }
+                        sectionReq.push(andRequirement);
+                    }
+                }
+            }
+        });
+        if (orRequirement !== "") {
+            sectionReq.push(orRequirement);
+        }
+        if (sectionReq.length !== 0) {
+            major.push(sectionReq);
+            sectionReq = []
+        }
+        console.log("--------------------Scraped major data--------------------");  
+        console.log(major);
+        resolve(major);
+    });
+}
+catalogToMajor('http://catalog.northeastern.edu/archive/2018-2019/undergraduate/computer-information-science/computer-science/bscs/#programrequirementstext')
